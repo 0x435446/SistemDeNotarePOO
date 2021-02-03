44884f04a2c60f64b89516f04543d2d07821b25f
flrpwr <vlad@gmail.com>
2021-02-03 16:54:20 +0200

    🚀 Deployed the project
    Signed-off-by: flrpwr <vlad@gmail.com>

--- modules/csv_module.py
+++ modules/csv_module.py
@@ -2,6 +2,7 @@ import csv
 from root import *
 
 
+#This module generates a csv dataset with projects features
 def print_NormalCsv(x,csvPath):
 	with open(csvPath, mode='w',newline='') as studenti_file:
 		studenti_writer = csv.writer(studenti_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
@@ -9,6 +10,7 @@ def print_NormalCsv(x,csvPath):
 		for i in range(len(x)):
 			studenti_writer.writerow(x[i].prints())
 
+#This module genereates a csv dataset with projects features + grading --> Will be used for training
 def print_DataSetCsv(x, csvPath):
 	with open(csvPath, mode='w',newline='') as studenti_file:
 		studenti_writer = csv.writer(studenti_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
