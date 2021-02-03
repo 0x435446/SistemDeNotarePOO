892e2580180b9b2a05e44aad2dd1ed5b907522ba
flrpwr <vlad@gmail.com>
2021-02-03 18:24:12 +0200

    📝Added Documentation
    Signed-off-by: flrpwr <vlad@gmail.com>

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from modelRegresie import *
 from studentGrader import *
 
 #Creating the dataset for train
-generateDatasetCsv("./DataSets/train","./DataSets/labels.txt","./resources/OOPHomeworkTrain.csv")
+#generateDatasetCsv("./DataSets/train","./DataSets/labels.txt","./resources/OOPHomeworkTrain.csv")
 
 #Creating the csv for testing --> No Grades
-generateNormalCsv("./DataSets/test","./resources/OOPHomeworkTest.csv")
+#generateNormalCsv("./DataSets/test","./resources/OOPHomeworkTest.csv")
 
 #Generating the regression model
 modelRegresie = generateModel("./resources/OOPHomeworkTrain.csv",visual=True)
